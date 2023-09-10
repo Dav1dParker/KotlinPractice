2bd1bf2647338f93b919168b056c2a4c1df076f7
Andrey <smail2204973@gmail.com>
2023-09-10 21:47:32 +0300

    Added localization

--- app/src/main/java/com/example/kotlinpr1/FirstFragment.kt
+++ app/src/main/java/com/example/kotlinpr1/FirstFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinpr1
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlinpr1.databinding.FragmentFirstBinding
+import java.util.Locale
 
 
+@Suppress("DEPRECATION")
 class FirstFragment : Fragment() {
 
     private lateinit var binding: FragmentFirstBinding
+    private var currentLanguage = "en"
+    lateinit var locale: Locale
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
