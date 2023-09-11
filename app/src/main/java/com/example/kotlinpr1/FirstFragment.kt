9ef1a0b89b0bf7fa123b02f7dffd62c4217fe36c
andrey <smail2204973@gmail.com>
2023-09-11 09:59:35 +0300

    bugfixed

--- app/src/main/java/com/example/kotlinpr1/FirstFragment.kt
+++ app/src/main/java/com/example/kotlinpr1/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.example.kotlinpr1.databinding.FragmentFirstBinding
@@ -56,6 +57,7 @@ class FirstFragment : Fragment() {
             transaction.replace(R.id.fragment_container_view, SecondFragment())
             transaction.addToBackStack(null)
             transaction.commit()
+            //Toast.makeText(context, "Hi",Toast.LENGTH_LONG).show()
         }
 
         binding.ThirdFragmentBtn.setOnClickListener {
